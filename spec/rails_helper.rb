b8e38b0ac7a5cc478b50b8a2bf448affa3bef914
Enzine <sonja.makipaja@kolumbus.fi>
2017-05-15 03:12:10 +0300

    coveralls not been updating for 4 hours, trying to fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,11 @@
 require 'simplecov'
 require 'coveralls'
-SimpleCov.start('rails')
+Coveralls.wear!('rails')
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'app/secrets'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
